2b3e242bb767b59b1a9d50eddf3583ed7d6339b0
mdfk <mdfk13@gmail.com>
2014-03-15 17:17:46 +0100

    Initial commit

---
+++ InterpretScript/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterpretScript
+{
+    public partial class MainWindow : Window
+    {
+        Script Script;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Script = new Script();
+        }
+
+        private void RunScript_Click(object sender, RoutedEventArgs e)
+        {
+            Script.RunScript(new TextRange(ScriptText.Document.ContentStart, ScriptText.Document.ContentEnd).Text);
+            ResultText.Document.Blocks.Clear();
+
+            ResultText.SelectAll();
+            ResultText.Selection.Text = Script.GetResult;
+        }
+    }
+}
---
+++ InterpretScript/Script.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpretScript
+{
+    class Script
+    {
+        string Result;
+        string Source;
+
+        public Script()
+        {
+            this.Result = string.Empty;
+            this.Source = string.Empty;
+        }
+
+        public string GetResult
+        {
+            get
+            {
+                return this.Result;
+            }
+        }
+
+        public void RunScript(string Source)
+        {
+            this.Source = Source;
+
+            // Compile source
+
+            this.Result = Source;
+        }
+    }
+}
