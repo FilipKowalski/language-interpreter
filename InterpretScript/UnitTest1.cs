f0e687641fe89b18f1edf9365da2d0f62698d9c3
Aderos <aderos@outlook.com>
2014-03-28 12:43:41 +0100

    Add refense for test

---
+++ InterpretScript/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterpretScript;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            String input = "int p = 2; if(p == 2){p = 4}";
+            int p = 2;
+            if (p == 2)
+                {
+                    p = 4;
+                }
+
+            // assert
+            //int actual = Interpreter(input);
+            //Assert.AreEqual(expected, actual, "Wrong interpreter response");
+        }
+    }
+}
