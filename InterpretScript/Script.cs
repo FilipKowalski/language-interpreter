61d96fc43a8689dc06af55d94e541db6fa50728f
Aderos <aderos@outlook.com>
2014-03-28 22:42:47 +0100

    Add class for variables

--- InterpretScript/Script.cs
+++ InterpretScript/Script.cs
@@ -8,6 +8,9 @@ namespace InterpretScript
 {
     class Script
     {
+        List<INT> listINT;
+        List<BOOL> listBOOL;
+        List<FLOAT> listFLOAT;
         string Result;
         string Source;
 
@@ -15,6 +18,9 @@ namespace InterpretScript
         {
             this.Result = string.Empty;
             this.Source = string.Empty;
+            this.listINT = new List<INT>();
+            this.listBOOL = new List<BOOL>();
+            this.listFLOAT = new List<FLOAT>();
         }
 
         public string GetResult
@@ -30,8 +36,34 @@ namespace InterpretScript
             this.Source = Source;
 
             // Compile source
+            INT a = new INT(5);
+            listINT.Add(a);
+            INT b = new INT(5);
+            listINT.Add(b);
+            a += b;
 
-            this.Result = Source;
+            INT a1 = new INT(5);
+            listINT.Add(a1);
+            INT b1 = new INT(5);
+            listINT.Add(b1);
+            a1 -= b1;
+
+            INT a2 = new INT(5);
+            listINT.Add(a2);
+            INT b2 = new INT(5);
+            listINT.Add(b2);
+            a2 *= b2;
+
+            INT a3 = new INT(5);
+            listINT.Add(a3);
+            INT b3 = new INT(5);
+            listINT.Add(b3);
+            a3 /= b3;
+
+            this.Result = a.Value.ToString() + "\n";
+            this.Result += a1.Value.ToString() + "\n";
+            this.Result += a2.Value.ToString() + "\n";
+            this.Result += a3.Value.ToString() + "\n";
         }
     }
 }
---
+++ InterpretScript/Variables.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpretScript
+{
+    class INT
+    {
+        private int value { get; set; }
+
+        public INT() { this.value = 0; }
+
+        public INT(int val) { this.value = val; }
+
+        public int Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public static INT operator +(INT a, INT b) { return new INT(a.Value + b.Value); }
+
+        public static INT operator -(INT a, INT b) { return new INT(a.Value - b.Value); }
+
+        public static INT operator *(INT a, INT b) { return new INT(a.Value * b.Value); }
+
+        public static INT operator/(INT a, INT b) { return new INT(a.Value / b.Value); }
+    }
+
+    public class BOOL
+    {
+        private bool value { get; set; }
+
+        public BOOL() { this.value = false; }
+
+        public BOOL(bool val) { this.value = val; }
+
+        public bool Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+    }
+
+    public class FLOAT
+    {
+        private float value { get; set; }
+
+        public FLOAT() { this.value = 0; }
+
+        public FLOAT(float val) { this.value = val; }
+
+        public float Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public static FLOAT operator +(FLOAT a, FLOAT b) { return new FLOAT(a.Value + b.Value); }
+
+        public static FLOAT operator -(FLOAT a, FLOAT b) { return new FLOAT(a.Value - b.Value); }
+
+        public static FLOAT operator *(FLOAT a, FLOAT b) { return new FLOAT(a.Value * b.Value); }
+
+        public static FLOAT operator/(FLOAT a, FLOAT b) { return new FLOAT(a.Value / b.Value); }
+    }
+}
