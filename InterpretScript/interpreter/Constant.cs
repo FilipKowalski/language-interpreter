af9b6ebffb20b966dc480e79d2db86f01ccd85ac
mdfk <mdfk13@gmail.com>
2014-03-30 22:35:48 +0200

    Interpreter step1

---
+++ InterpretScript/interpreter/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpretScript
+{
+    class Constant : Expression
+    {
+        int value;
+
+        public Constant(int v) {
+            value = v;
+        }
+    }
+}
